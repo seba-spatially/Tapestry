62879520fa3a2bae413a5f3243f2bc3e5d54207b
seba <sebastian@spatially.com>
2017-05-17 17:50:25 -0400

    code is ready for running 1 MSA at the time

--- tapestry2.py
+++ tapestry2.py
@@ -1,17 +1,34 @@
+#Due to the multiprocessing method used below  I can't wrap the whole process in a function without major surgery
+#Therefore the way to generate tapestries is to define the variable "msa" with the name of the MSA we want to process
+#and then run the entire script.
+
+
+#Make sure the variable "msa" uniquely identifies an MSA in  the query:
+# 'metro/tiger/2014/1/tl_2014_us' and data['t_name'] like 'msa%';
+
+msa = 'Miami'
+
+import timeit
+from multiprocessing import Pool
 import sqlalchemy as sa
 import pandas as pd
+from shapely.geometry import shape
+from shapely.wkt import dumps
+import geopandas as gpd
+
+start_time = timeit.default_timer()
 
 engine = sa.create_engine("crate://db.world.io:4200")
-msa = 'Boston'
-q = """select wkt['solid']
-from shaped
-where rowid = (select rowid from world where datasetid='metro/tiger/2014/1/tl_2014_us' and data['t_namelsad'] like '%{}%') limit 10;
+
+#####Get the MSA shape
+q = """select shape from world
+where datasetid='metro/tiger/2014/1/tl_2014_us'
+and data['t_name'] like '{}%';
 """.format(msa)
 z = pd.read_sql(q, engine)
-BB = z["wkt['solid']"][0]
+BB = dumps(shape(z['shape'][0]))
 
-
-#######blocks in BB
+#### Get the Tiger blocks that intersect an MSA
 q = """select count(*) from world
     where ingestid = '25cb2427c0a5710a983e2f3a1f484bc6'
      and match(shape, '{}') using intersects""".format(BB)
@@ -22,14 +39,12 @@ q = """select shape, data['t_geoid10'] geoid10  from world
      and match(shape, '{}') using intersects limit {}""".format(BB, z['count(*)'][0])
 blocks = pd.read_sql(q, engine)
 
-from shapely.geometry import shape
+##  Make a geodataframe wth the blocks
 geo = [shape(i) for i in blocks['shape']]
 del blocks['shape']
-
-import geopandas as gpd
 blocks.gpd = gpd.GeoDataFrame(geometry=geo, data=blocks, crs={'init': 'epsg:4326'})
 
-###### Housing Units
+###### Get housing data
 q = """select count(*)  from world
         where ingestid = '139fa87c22976bbd156da6dd9ab672ec'
         and point is not null
@@ -41,11 +56,12 @@ q = """select shape, data['t_propertygroup'] propertygroup, data['t_propertytype
         and point is not null
         and match(shape, '{}')using intersects limit {}""".format(BB, z['count(*)'][0])
 houses = pd.read_sql(q,engine)
-
+#Make a geodataframe with the housing data
 geo = [shape(i) for i in houses['shape']]
 del houses['shape']
 houses.gpd = gpd.GeoDataFrame(geometry=geo, data=houses, crs={'init': 'epsg:4326'})
 
+#Select residential points only
 h = ['Single Family Residence','Condominium', 'Triplex (3 units, any combination)',
                                'Multi-Family Dwellings (Generic, any combination)',
                                'Duplex (2 units, any combination)', 'Quadplex (4 Units, Any Combination)',
@@ -56,16 +72,18 @@ houses.gpd['type'] = 'residential'
 del houses.gpd['propertygroup']
 del houses.gpd['propertytype']
 
-###### Biz dataselect * from sys.jobs limit 100;
-
+###### Get business data
+msa = msa.lower()
+#Get ingestid and datasetid of the business data for the msa processed
 def bizLoc(msa=msa):
     import sqlalchemy as sa
     import pandas as pd
     engine = sa.create_engine("crate://db.world.io:4200")
-    msa = 'boston'
-    q = "select datasetid, metadata['table'] _table, ingestid, major, minor "\
-    + "from dataset where datasetid like 'business%metro-{}' ".format(msa)\
-    + "order by datasetid, major desc, minor desc"
+    # This query might fail.  make sure that the variable msa defined above can also uniquely identify the datasetid
+    # of the business data in the desired MSA
+    q = """select datasetid, metadata['table'] _table, ingestid, major, minor
+    from dataset where datasetid like 'business/spatially/201705/2/metro-{}'
+    order by datasetid, major desc, minor desc""".format(msa.lower()[0:8])
     df = pd.read_sql(q, engine)
     df.head()
     x = df.loc[df.major == df.major.max()]
@@ -74,7 +92,7 @@ def bizLoc(msa=msa):
     out = {'ingestID':x.ingestid[0], 'table':x._table[0]}
     return(out)
 
-bl = bizLoc('boston')
+bl = bizLoc(msa)
 q = """select count(*) from {}
         where ingestid = '{}'""".format(bl['table'], bl['ingestID'])
 z = pd.read_sql(q, engine)
@@ -86,21 +104,20 @@ where a.ingestid = '{}'
 limit {}
 """.format(bl['table'], bl['ingestID'], z['count(*)'][0]*10)
 
-from shapely.geometry import shape
-import geopandas as gpd
+
 biz = pd.read_sql(q, engine)
 geo = [shape(i) for i in biz['shape']]
 del biz['shape']
 biz.gpd = gpd.GeoDataFrame(geometry=geo, data=biz, crs={'init': 'epsg:4326'}).copy()
-biz.gpd.to_csv('bizBoston_w_sic.tsv', index=False, sep='\t')
-print('done')
 
-from pandasql import *
+####label the business data into the types used in tapestry (uses SIC and is_restaurant)
+from pandasql import sqldf
 del biz['geometry']
 biz.gpd['type'] = ''
 
 pysqldf = lambda q: sqldf(q, globals())
 
+###Professionals
 q = """
 select pid from biz
 where sic like '81%'
@@ -112,6 +129,7 @@ k = pysqldf(q)
 key = biz.gpd.loc[biz.gpd.pid.isin(tuple(k.pid))].index.tolist()
 biz.gpd.set_value(index=key, col='type', value='professional')
 
+###Retail
 q = """
 select pid from biz
 where sic like '53%'
@@ -124,6 +142,7 @@ k = pysqldf(q)
 key = biz.gpd.loc[biz.gpd.pid.isin(tuple(k.pid))].index.tolist()
 biz.gpd.set_value(index=key, col='type', value='retail')
 
+###Hotel
 q = """
 select pid from biz
 where sic like '701%';
@@ -132,6 +151,7 @@ k = pysqldf(q)
 key = biz.gpd.loc[biz.gpd.pid.isin(tuple(k.pid))].index.tolist()
 biz.gpd.set_value(index=key, col='type', value='hotel')
 
+###Culture and entertainment
 q = """
 select pid from biz
 where sic like '793%'
@@ -143,6 +163,7 @@ k = pysqldf(q)
 key = biz.gpd.loc[biz.gpd.pid.isin(tuple(k.pid))].index.tolist()
 biz.gpd.set_value(index=key, col='type', value='culture_entertainment')
 
+###Government
 q = """
 select pid from biz
 where sic like '91%'
@@ -152,6 +173,7 @@ k = pysqldf(q)
 key = biz.gpd.loc[biz.gpd.pid.isin(tuple(k.pid))].index.tolist()
 biz.gpd.set_value(index=key, col='type', value='government')
 
+###Restaurant
 q = """
 select pid from biz
 where is_restaurant =1;
@@ -171,18 +193,11 @@ del biz.gpd['sic']
 points = houses.gpd.append(biz.gpd, ignore_index=True)
 points = points.reset_index(drop=True)
 
-#base = blocks.gpd.plot(color='white')
-#points.plot(ax=base, marker='o', color='red', markersize=5)
-
-###### Intersection
-
+###### Intersection points w. blocks
 from rtree import index
 idx = index.Index()
 for index, row in blocks.gpd.iterrows():
     idx.insert(index,row.geometry.bounds)
-
-import timeit
-start_time = timeit.default_timer()
 
 def seba_over(point):
     poly_idx = [i for i in idx.intersection((point.coords[0]))
@@ -193,14 +208,11 @@ def seba_over(point):
         out = {'polyID':-999}
     return(out)
 
-import timeit
-from multiprocessing import Pool
-start_time = timeit.default_timer()
+
 if __name__ == '__main__':
     with Pool(8) as p:
         poly_ids = p.map(seba_over, points.geometry)
-elapsed = timeit.default_timer() - start_time
-print(elapsed/60)
+
 poly_ids = pd.DataFrame(poly_ids)
 poly_ids.head()
 
@@ -213,15 +225,17 @@ y['blockID'] = blocks.gpd.index
 data = x.merge(y, on='blockID')
 data['values'] = 1
 
-
 pt = data.groupby(['geoid10', 'type'])['values'].sum().unstack()
 pt = pt.fillna(value=0)
 def f(x):
     return(x/x.max())
 pt = pt.apply(f, axis=0)
 pt = pt.apply(f, axis=1)
+
 from numpy import argmax
 labels = pd.DataFrame(data={'type':pt.apply(argmax,1)})
 labels['geoid10'] = labels.index
 blocks.gpd = blocks.gpd.merge(labels, on='geoid10', how='left')
-blocks.gpd.to_file('TapestryTest2.geojson', driver='GeoJSON')
+blocks.gpd.to_file('Tapestry_{}.geojson'.format(msa), driver='GeoJSON').format()
+elapsed = (timeit.default_timer() - start_time) / 60
+print('{} completed in {} minutes'.format(msa, elapsed))
